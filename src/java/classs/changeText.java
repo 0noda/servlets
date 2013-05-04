98060e7e13ed95079a248d2fda84dfb7e453bc95
0noda <0n0da@mail.ru>
2013-05-05 00:48:19 +0400

    add class

---
+++ src/java/classs/changeText.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classs;
+
+/**
+ *
+ * @author ivanov
+ */
+public class changeText {
+    public static String change(String text)
+    {
+    return text+"123";
+    }
+}
